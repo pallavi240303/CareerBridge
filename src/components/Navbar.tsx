2d2cd53d9d18be0bfa2407c4b2556214f7ba63d0
pallavi240303 <24.pallavi.2003@gmail.com>
2024-06-27 06:27:11 +0530

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/assets/logo.png"
+import { Button } from "./ui/button";
+export default function Navbar() {
+  return (
+    <header className="shadow-sm ">
+      <nav className="max-w-5xl m-auto py-5 px-3 flex items-center justify-between">
+        <Link href="/" className="flex items-center gap-3">
+            <Image
+                src={logo}
+                width={40}
+                height={40}
+                alt="logo"
+            />
+            <span className="text-xl font-bold tracking-tight">Career Bridge</span>
+        </Link>
+        <Button asChild>
+            <Link href="/jobs/new">Post a Job</Link>
+        </Button>
+      </nav>
+    </header>
+  )
+}
